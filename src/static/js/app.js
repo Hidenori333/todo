b0b9c3d3b1033ee2c15cb9b5b80d317ff9f79a61
Nori756 <kiyohide826@gmail.com>
2021-12-27 17:43:30 +0900

    8 commit

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,11 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+              <Row>
+                <Col md={{ offset: 3, span: 6 }}>
+                    <h1>ToDO App by Hidenori Kiyomoto</h1>
+                </Col>
+            </Row>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
